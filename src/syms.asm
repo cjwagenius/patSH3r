823f9363ccbf28554a55d08e7467481868d38344
C.J.Wagenius <cjw@voidptr.se>
2018-11-21 20:53:48 +0100

    Added crew struct

--- src/syms.asm
+++ src/syms.asm
@@ -11,6 +11,43 @@
 %define ASM_JMP		0xe9
 %define	ASM_RET		0xc3
 
+struc crew 
+	.name		resb	52 ;   -(uses only 50)
+	.index		resd	 1 ;  0-integer
+	.nrcomp		resd	 1 ;  4-integer
+	.renown		resd	 1 ;  8-float
+	.patrols	resd	 1 ;  c-integer
+	.type		resd	 1 ; 10-integer
+	.grad		resd	 1 ; 14-integer
+	.notused_a	resd	 1 ; 18 ? dword
+	.morale		resd	 1 ; 1c-float
+	.fatigue	resd	 1 ; 20-float
+	.healthsts	resd	 1 ; 24-integer
+	.waswounded	resd	 1 ; 28-integer
+	.experience	resd	 1 ; 2c-float
+	.rank		resd	 1 ; 30-integer
+	.nrqual		resd	 1 ; 34-integer
+	.quals		resd	 3 ; 38-integer
+	.specability	resd	 1 ; 44-integer
+	.medals		resd	 9 ; 48-integer
+	.hitpoints	resd	 1 ; 6c-float
+	.notused_b	resd	 1 ; 70 ? dword
+	.notused_c	resd	 1 ; 74 ? dword
+	.notused_d	resd	 1 ; 78 ? dword
+	.headidx	resd	 1 ; 7c-integer
+	.headtgaidx	resd	 1 ; 80-integer
+	.bodyidx	resd	 1 ; 84-integer
+	.bodytgaidx	resd	 1 ; 88-integer
+	.hashelmet	resd	 1 ; 8c-integer
+	.helmetidx	resd	 1 ; 90-integer
+	.helmettgaidx	resd	 1 ; 94-integer
+	.voiceIdx	resd	 1 ; 98-integer
+	.prom		resb	 1 ; 9c-byte
+	.medal		resb	 9 ; 9d-byte
+	.qual		resb	 9 ; a6-byte
+	.notused_e	resb	 1 ;
+endstruc
+
 ; --- Win32 -------------------------------------------------------------------
 extern _sprintf
 extern _snprintf
@@ -36,9 +73,11 @@ extern _fmgr_get_dbl	; get double from ini-file
 extern _fmgr_get_str	; get string from ini-file
 
 ; variables
+extern _crewofs		; offset to crew-array pointer
 extern _fmgrofs		; offset to filemanager.dll
 extern _maincfg		; offset to main.cfg object
 
 ; --- patches.asm -------------------------------------------------------------
 extern _ptc_version_init
 extern _ptc_smartpo_init
+
